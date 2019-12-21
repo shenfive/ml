69ad7b7b827f9574c1d548630f5864e82f3363cf
Danny Shen <shenfive@gmail.com>
2019-12-21 16:37:06 +0800

    多語自動化

---
+++ ml/utility.swift
@@ -0,0 +1,15 @@
+//
+//  utility.swift
+//  ml
+//
+//  Created by 申潤五 on 2019/12/21.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import Foundation
+
+extension String{
+    func localize()->String{
+        return NSLocalizedString(self, comment: "")
+    }
+}

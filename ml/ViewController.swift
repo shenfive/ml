eb88f1e53511d48047477c7fd2a4955bf770ba5f
Danny Shen <shenfive@gmail.com>
2019-12-21 16:25:47 +0800

    ml

--- ml/ViewController.swift
+++ ml/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(NSLocalizedString("key", comment: ""))
         // Do any additional setup after loading the view.
     }
 
